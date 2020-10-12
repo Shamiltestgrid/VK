f8da919a2b01458c066d8172b155065646d807ee
Shamiltestgrid <60189855+Shamiltestgrid@users.noreply.github.com>
2020-10-12 15:28:39 +0300

    Update CommentsInfo.java

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -2,7 +2,7 @@ package ru.netology.domain;
 
 public class CommentsInfo {
     private int count;
-    private int canPost;
+    private boolean canPost;
     private int groupsCanPost;
     private boolean canClose;
 
